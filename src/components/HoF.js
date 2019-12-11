2464815be26a93f15a3bd913221a88018ac27151
Filip Bendevski <bende@localhost.localdomain>
2019-12-11 09:58:08 +0800

    likes

--- src/components/HoF.js
+++ src/components/HoF.js
@@ -3,7 +3,7 @@ import Post from './Post';
 import Wrapper from '../flexing';
 export default function(props){
     console.log(props.posts)
-    const hold = props.posts.slice(0).sort((a,b)=>parseInt(a.likes)>parseInt(b.likes)? -1:1).map((post)=><Post key={post.id} comments={post.comments} content={post.content} title={post.title}/>);
+    const hold = props.posts.slice(0).sort((a,b)=>parseInt(a.likes)>parseInt(b.likes)? -1:1).map((post)=><Post key={post.id} likes={post.likes} comments={post.comments} content={post.content} title={post.title}/>);
     return(
         <Wrapper>
             {hold}
